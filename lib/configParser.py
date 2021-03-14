1e9ff49a09cdb5f91b965bbff3bbe5f8a5b86799
Nisi@Parrot <unconfigured@null.spigotmc.org>
2021-03-14 18:30:08 +0100

    first steps for config

--- lib/configParser.py
+++ lib/configParser.py
@@ -1,17 +1,20 @@
 import os
 
 from dotenv import load_dotenv
+from tinydb import TinyDB, Query
 
 class cfg:
     def __init__(self):
         load_dotenv()
         self.RiotToken = os.getenv("RiotToken")
-    
-    def get(self,MySQL,key:str):
-        return MySQL.execute("select value from config where CKey = '{}';".format(key),False,True)
+        self.CDB = TinyDB('./db/config.json')
+        self.query = Query()
 
-    def set(self,MySQL,key:str,value:str,log:bool=False,read:bool=False): # ill first try checking if the value exist to use update if it does.
-        if not MySQL.execute("SELECT value from config where CKey = '{}';".format(key),log,True): # assume its a new value if it returns -1
-            MySQL.execute("INSERT INTO config (CKey,value) VALUES ('{}','{}');".format(key,value),log,read,True) # using insert to add the new value.
+    def get(self,key:str):
+        return self.CDB.search(self.query.key == key)
+
+    def set(self,key:str,value:str): # ill first try checking if the value exist to use update if it does.
+        if str(self.CDB.search(self.query.key == key)) == "[]": # assume its a new value
+            self.CDB.insert({'key':key,'value':value}) # using insert to add the new value.
         else:
             MySQL.execute("UPDATE config SET Value = '{}' where CKey = '{}';".format(value,key),log,read,True) # updating it.
