ffe90e42010cd7f193a4cddd3cb068be5eb6e4bd
Nisi@win10 <wolf.sf75@googlemail.com>
2021-03-29 12:36:37 +0200

    lol

--- Tournament.py
+++ Tournament.py
@@ -1,1 +1,18 @@
-#keep
+import discord
+
+from lib.Riot import PlayerClass
+
+client = discord.Client()
+PlayerManager = PlayerClass()
+
+@client.event
+async def on_ready():
+    ZéroTwó = await PlayerManager.getPlayer("ZéroTwó")
+    print(await ZéroTwó.VerifyPlayer('HalloNami'))
+
+
+@client.event
+async def on_message(msg):
+    print(msg)
+
+client.run('NzY4OTUxMTk5MDI5NjU3NjIx.X5H7nA.LEpGSPLhM6HNcVXAAd-xzv3W348')
